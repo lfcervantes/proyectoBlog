62f41f04944b0835c676c347118d799f86eb8185
lfcervantes <65520144+lfcervantes@users.noreply.github.com>
2020-05-17 22:28:38 -0500

    Primera version
    Se sube la primera version del proyecto

---
+++ portada-python.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def url_principal():
+	return render_template("", estudiante="Luis Fernando Hernandez Cervantes")
+	
+
+if __name__=="__main__":
+	app.run(debug=True)
